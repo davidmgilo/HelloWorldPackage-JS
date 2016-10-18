0e70e31f45ea35ea159506d06cbec057662b35bd
David Martinez <davidmgilo@gmail.com>
2016-10-18 17:05:55 +0200

    explicacions

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hello world!');
+function say (){
+    console.log('Hello world!');
+}
+
+//ENCAPSULAMENT : relacions amb l'exterior. Amagar estat intern dels objectes. Per canviar l'estat (propietats) obliguem a utilitzar funcions
+//SETTER/GETTER => Evitar. No són expressius. Cal? Trobar noms millors.
+//ENCAPSULAMENT: per dissenyar millor codi.  Per evitar code coupling / Acoblament de codi.
+//
+// Dependre d'especificacions/interfícies i no d'implementacions.
+// ENCAPSULAR -> 1) Amagar implementació 2) Definir la interfície -> API Pública
